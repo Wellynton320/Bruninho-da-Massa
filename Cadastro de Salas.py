a475d6e3b7a40037fa7e0e6f223e4eacec150db2
Wellynton <wellyntomotpech77@gmail.com>
2024-05-18 20:16:58 -0300

    changes

--- Cadastro de Salas.py
+++ Cadastro de Salas.py
@@ -11,7 +11,7 @@ def validar_numeros(input):
 def cadastrar_sala():
     nome = nome_entry.get()
     capacidade = capacidade_entry.get()
-    localizacao = localizacao_var.get()  # Obtemos o valor selecionado na combobox
+    localizacao = localizacao_var.get()
     equipamentos = equipamentos_entry.get().split(',')
 
     sala = {
@@ -34,13 +34,11 @@ def main():
     frame = tk.Frame(root, padx=20, pady=20)
     frame.pack()
 
-    # Labels
     tk.Label(frame, text="Nome da sala:").grid(row=0, column=0, sticky="e")
     tk.Label(frame, text="Capacidade da sala:").grid(row=1, column=0, sticky="e")
     tk.Label(frame, text="Localização da sala:").grid(row=2, column=0, sticky="e")
     tk.Label(frame, text="Equipamentos disponíveis (separados por vírgula):").grid(row=3, column=0, sticky="e")
 
-    # Entries
     global nome_entry, capacidade_entry, localizacao_var, equipamentos_entry
     nome_entry = tk.Entry(frame)
     capacidade_entry = tk.Entry(frame, validate="key")
@@ -48,14 +46,13 @@ def main():
     equipamentos_entry = tk.Entry(frame)
     
     localizacao_var = tk.StringVar(root)
-    localizacao_combobox = tk.OptionMenu(frame, localizacao_var, "Prédio Sede", "CTU")
+    localizacao_combobox = tk.OptionMenu(frame, localizacao_var, "Prédio Sede", "Prédio CTU")
     localizacao_combobox.grid(row=2, column=1, sticky="w")
 
     nome_entry.grid(row=0, column=1)
     capacidade_entry.grid(row=1, column=1)
     equipamentos_entry.grid(row=3, column=1)
 
-    # Botão para cadastrar sala
     cadastrar_button = tk.Button(frame, text="Cadastrar Sala", command=cadastrar_sala)
     cadastrar_button.grid(row=4, columnspan=2)
 
