c0fad2a7099a53cf19a10e79459a3cfeb23aaa75
Wellynton <wellyntomotpech77@gmail.com>
2024-05-18 20:08:14 -0300

    changes

--- Cadastro de Salas.py
+++ Cadastro de Salas.py
@@ -1,11 +1,18 @@
-#Cadastro de Salas
 import json
+import tkinter as tk
+from tkinter import messagebox
+
+def validar_numeros(input):
+    if input.isdigit() or input == "":
+        return True
+    else:
+        return False
 
 def cadastrar_sala():
-    nome = input("Nome da sala: ")
-    capacidade = input("Capacidade da sala: ")
-    localizacao = input("Localização da sala: ")
-    equipamentos = input("Equipamentos disponíveis (separados por vírgula): ").split(',')
+    nome = nome_entry.get()
+    capacidade = capacidade_entry.get()
+    localizacao = localizacao_var.get()  # Obtemos o valor selecionado na combobox
+    equipamentos = equipamentos_entry.get().split(',')
 
     sala = {
         "nome": nome,
@@ -18,19 +25,41 @@ def cadastrar_sala():
         json.dump(sala, file)
         file.write('\n')
     
-    print("Sala cadastrada com sucesso!")
+    messagebox.showinfo("Sucesso", "Sala cadastrada com sucesso!")
 
 def main():
-    print("===== Cadastro de Salas =====")
-    while True:
-        opcao = input("1 - Cadastrar Sala\n0 - Sair\nEscolha uma opção: ")
+    root = tk.Tk()
+    root.title("Cadastro de Salas")
 
-        if opcao == '1':
-            cadastrar_sala()
-        elif opcao == '0':
-            break
-        else:
-            print("Opção inválida. Tente novamente.")
+    frame = tk.Frame(root, padx=20, pady=20)
+    frame.pack()
+
+    # Labels
+    tk.Label(frame, text="Nome da sala:").grid(row=0, column=0, sticky="e")
+    tk.Label(frame, text="Capacidade da sala:").grid(row=1, column=0, sticky="e")
+    tk.Label(frame, text="Localização da sala:").grid(row=2, column=0, sticky="e")
+    tk.Label(frame, text="Equipamentos disponíveis (separados por vírgula):").grid(row=3, column=0, sticky="e")
+
+    # Entries
+    global nome_entry, capacidade_entry, localizacao_var, equipamentos_entry
+    nome_entry = tk.Entry(frame)
+    capacidade_entry = tk.Entry(frame, validate="key")
+    capacidade_entry['validatecommand'] = (capacidade_entry.register(validar_numeros), '%S')
+    equipamentos_entry = tk.Entry(frame)
+    
+    localizacao_var = tk.StringVar(root)
+    localizacao_combobox = tk.OptionMenu(frame, localizacao_var, "Prédio Sede", "CTU")
+    localizacao_combobox.grid(row=2, column=1, sticky="w")
+
+    nome_entry.grid(row=0, column=1)
+    capacidade_entry.grid(row=1, column=1)
+    equipamentos_entry.grid(row=3, column=1)
+
+    # Botão para cadastrar sala
+    cadastrar_button = tk.Button(frame, text="Cadastrar Sala", command=cadastrar_sala)
+    cadastrar_button.grid(row=4, columnspan=2)
+
+    root.mainloop()
 
 if __name__ == "__main__":
     main()
